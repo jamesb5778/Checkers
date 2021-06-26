e6efea10a402a6e2027dfbb40c44143c9963837b
James Bell <jamesbell@Jamess-MacBook-Pro.local>
2021-06-26 16:40:06 -0700

    change html checkboard to table instead of dev

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//Checkeboard layout and starting positions
+const checkerboard = [
+    null, "RC-1", null, "RC-2", null, "RC-3", null, "RC-4",
+    "RC-5", null, "RC-6", null, "RC-7", null, "RC-8", null,
+    null, "RC-9", null, "RC-10", null, "RC-11", null, "RC-12",
+    null, null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null, null,
+    "BC-1", null, "BC-2", null, "BC-3", null, "BC-4", null,
+    null, "BC-5", null, "BC-6", null, "BC-7", null, "BC-8",
+    "BC-9", null, "BC-10", null, "BC-11", null, "BC-12", null
+]
+
